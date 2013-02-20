e9cc643d4b2fabb9ba9dcc1b78705c8af12a1a4f
Graham Leslie <gleslie2008@gmail.com>
2013-02-20 14:06:28 -0600

    added get value fcns

--- db315/db315/Record.cpp
+++ db315/db315/Record.cpp
@@ -17,8 +17,46 @@ void Record::removeValue(int i){
 	values.erase(values.begin()+i);
 }
 
-string Record::getValue(int i){
+string Record::getRawValue(int i){
 	return values[i];
+}
+
+/* 	
+	getting values:
+	0: "string"
+	1: "float"
+	2: "int"
+	3: "date"
+*/
+	
+string Record::getStringValue(int i) {
+	return values[i];
+}
+
+float Record::getFloatValue(int i) {
+	stringstream ss;
+	ss << record[i];
+	float f;
+	ss >> f;
+	return f;
+}
+
+int Record::getIntValue(int i) {
+	stringstream ss;
+	ss << record[i];
+	int iv;
+	ss >> iv;
+	return iv;
+}
+
+Date Record::getDateValue(int i) {
+	stringstream ss;
+	string date = record[i];
+	replace(date.begin(), date.end(), '/', ' ');
+	ss << date;
+	int m, d, y;
+	ss >> y >> m >> d;
+	return Date(y,m,d);
 }
 
 string Record::operator[](int i) const{
