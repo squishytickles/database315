9e54031aaf9d9a61ced734154d97a2828f93e501
Graham Leslie <gleslie2008@gmail.com>
2013-02-20 13:46:06 -0600

    added getTypeInt for query functions

--- db315/db315/AttributeTypeTuple.cpp
+++ db315/db315/AttributeTypeTuple.cpp
@@ -11,3 +11,20 @@ void AttributeTypeTuple::setAttribute(string newName){
 string AttributeTypeTuple::getType(){
 	return type;
 }
+
+int AttributeTypeTuple::getTypeInt(){
+	/* 	
+		for use by query fcns in database:
+		0: "string"
+		1: "float"
+		2: "int"
+		3: "date"
+	*/
+	if (type.compaare("string" == 0)) 	return 0;
+	else if (type.compare("float" == 0))	return 1;
+	else if (type.compare("int" == 0))	return 2;
+	else if (type.compare("date" == 0))	return 3;
+	
+	// throw an exception if there is an invalid type stored
+	throw Database_exception("Unknown type stored in AttributeTypeTuple!");
+}
